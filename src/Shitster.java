4792b263b276922812d18178e0702d4d5be3835a
HFrejlev <henrikfrejlev@hotmail.com>
2019-09-13 14:19:51 +0200

    Rasmus' og Henriks lorteprojekt.

---
+++ src/Shitster.java
@@ -0,0 +1,26 @@
+public class Shitster {
+
+    //Variabler
+
+    float weight; //angiver din lorts vægt.
+    boolean health; //angiver om din lort er sund eller ej.
+    double temp; //angiver din lorts temperatur
+    String reminder; //fortæller dig, at du bør gå på toillet.
+
+    void erdensund() {
+        if (health) {
+            System.out.println("Sund");
+
+
+        }
+
+        else {
+            System.out.println("Dårlig");
+
+        }
+
+
+    }
+
+
+}
