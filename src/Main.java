d36c9a8caa0fe5ec67fb56f4b30b594494a3d0f7
András Ács <acsandras@gmail.com>
2019-09-13 13:35:31 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+/**
+ * Developed by András Ács (acsandras@gmail.com)
+ * Zealand / www.zealand.dk
+ * Licensed under the MIT License
+ * 2019-09-13
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("************************************");
+        System.out.println("****** Smarthome v. 0.2 2019 *******");
+        System.out.println("************************************");
+        System.out.println("");
+        System.out.println("Utroligt!");
+        System.out.println("Alle devices fra klassen samlet i et hjem!");
+
+    }
+}
